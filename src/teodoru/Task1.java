7025bc50d6345a7957b2316261cb5cc782e898df
teodoru marin <teodorumarin25@gmail.com>
2025-10-28 03:58:03 +0000

    Task1 Solved

--- src/teodoru/Task1.java
+++ src/teodoru/Task1.java
@@ -9,5 +9,28 @@ package teodoru;
       identify (6) --> "Even"
      */
 
+
 public class Task1 {
+    public static void main(String[] args) {
+
+
+        identify(5);
+        identify(6);
+        identify(157);
+
+    }
+
+
+    //  The method takes an integer input and doesn’t return anything (void), because it directly prints the result.
+    public static void identify(int number) {
+
+        if (number % 2 == 0) {  // If the number it's divisible by 2 with no reminder it's Even, otherwise it's Odd
+            System.out.println(number + " = Even");
+        } else {
+            System.out.println(number + " = Odd");
+        }
+
+    }
 }
+
+
