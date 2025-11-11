04ad52ab1917609e8b9a6867cec4f67a08716787
busranurartar <artarbusranur@gmail.com>
2025-11-11 14:54:59 -0500

    Comments added for Task3

--- src/busra/Task3.java
+++ src/busra/Task3.java
@@ -8,10 +8,13 @@ public class Task3 {
 
         int num1 = 10;
         int num2 = 15;
+        //Initially, num1 is 10 and num2 is 15.
 
-        num1 = num1 + num2; // num1 is 25
-        num2 = num1 - num2; // num2 is 10 now (num2 have num1 value )
-        num1 = num1 - num2; // num1 is 15 now (num1 have num2 value )
+        num1 = num1 + num2; // num1 becomes 25.
+        //Now I subtract the current value of num2 (which is 15) from num1 (which is 25).
+        num2 = num1 - num2; // The result is 10, and I assign it to num2,num2 holds the original value of num1.
+        //Finally, I subtract the new num2 (which is 10) from num1 (which is 25).
+        num1 = num1 - num2; // The result is 15, so num1 now holds the original value of num2.
 
         System.out.println("num1 = " + num1);
         System.out.println("num2 = " + num2);
